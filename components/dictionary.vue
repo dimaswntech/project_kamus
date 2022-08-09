03fe4a7268f2c1a5bfdfcabb73c64262f774aa9c
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-08-09 10:08:35 +0700

    first commit

--- components/dictionary.vue
+++ components/dictionary.vue
@@ -6,7 +6,7 @@
         <div class="box py-3 px-8 flex flex-col">
           <div class="flex justify-center">
             <div class=" inputSection relative">
-              <img @click="load" class="absolute z-20 left-0 top-0 h-[10rem]" src="images/logo_kbbi.png">
+              <img @click="load" class="absolute z-20 left-0 top-0 h-[10rem] cursor-pointer" src="images/logo_kbbi.png">
               <input v-model="kata" class="translate-y-1/2 inputKata h-[5rem] w-full"/>
             </div>
           </div>
@@ -15,14 +15,23 @@
             <p class="text-[90px] font-pacivico">{{ kata }}</p>
           </div>
         </div>
-        <div class="boks box row-span-2">
-          <ul v-for="maksud in artiData">
-            <template v-for="(arti, index) in maksud.meanings">
-              <p>{{ index + 1 }}</p>
-              <p><span class="font-bold">Part of speech :</span> {{ arti.partOfSpeech }}</p>
-              <!--              <p><span class="font-bold">Part of speech :</span> {{ arti.partOfSpeech }}</p>-->
-            </template>
+        <div class="boks box row-span-2 py-3 px-8 flex flex-col">
+          <span class="text-[50px] mt-2 font-josefin">Meanings</span>
+          <ul class="ml-3" v-for="maksud in artiData">
+            <div class="mb-2.5" v-for="arti in maksud.meanings">
+              <p><span class="font-bold">Part Of Speech</span> ( {{arti.partOfSpeech}} )</p>
+              <div v-for="(data, index) in arti.definitions" :key="index" class="flex mb-1.5 ml-1.5">
+                <p class="font-pacivico mr-2.5 text-xl">{{ index + 1 }}</p>
+                <p class="">{{ data.definition }}</p>
+              </div>
+            </div>
           </ul>
+
+          <span class="text-[50px] mt-2 font-josefin">License</span>
+          <div class="mb-2.5 ml-3" v-for="izin in artiData">
+            <p>{{izin.license.name}}</p>
+            <a>{{izin.license.url}}</a>
+          </div>
         </div>
         <div class="boks box py-3 px-8 flex flex-col">
           <span class="text-[50px] mt-2 font-josefin">Phonetics</span>
